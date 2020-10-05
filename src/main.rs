ec56d087f8a8aa9cb70c499c24b58fe5a6e515ca
Ingo Wechsung <ingo.wechsung@gmail.com>
2020-10-06 00:10:03 +0200

    implemented zobrist stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rasch::zobrist;
+
 fn main() {
-    println!("Hello, world!");
+    println!("ppfZobrist(1,6,0) {:#}", zobrist::ppfZobrist(1, 6, 0));
+    println!("value {:x}  !value {:x}", 0xaau8, !0xaau8);
 }
