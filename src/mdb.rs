690f0581c367ab7a70cdb898c3a33d33f0f6ffab
Ingo Wechsung <ingo.wechsung@gmail.com>
2020-10-06 09:17:02 +0200

    add all needed static arrays

--- src/mdb.rs
+++ src/mdb.rs
@@ -29,30 +29,41 @@ valid. Hence, 0 means the move is possible, and -1 means it is not possible (unl
 
 ///     the eight row
 const OBEN: u64 = 0xff00000000000000u64;
+
 ///     the H file
 const RECHTS: u64 = 0x8080808080808080u64;
+
 ///     the A file
 const LINKS: u64 = 0x0101010101010101u64;
+
 ///     the first row
 const UNTEN: u64 = 0x00000000000000ffu64;
+
 ///     when added to an index that is not on the H file, yields the index to the right, i.e. e5 -> f5
 const EAST: i32 = 1;
+
 ///     when added to an index that is not on the A file, yields the index to the left, i.e. e5 -> d5
 const WEST: i32 = -1;
+
 ///     when added to an index that is not on the first row, yields the index down one row, i.e e5 -> e4
 const SOUTH: i32 = -8;
+
 ///     when added to an index that is not on the last row, yields the index up one row, i.e e5 -> e6
 const NORTH: i32 = 8;
+
 ///     e5 -> f6
 const NE: i32 = NORTH + EAST;
+
 ///     e5 -> f4
 const SE: i32 = SOUTH + EAST;
+
 ///     e5 ->  d4
 const SW: i32 = SOUTH + WEST;
+
 ///     e5 ->  d6
 const NW: i32 = NORTH + WEST;
 
-/// indicate a legal move
+/// indicate a move that is not affected by other figures
 const LEGAL: u64 = 0;
 
 /// indicate an illegal move
@@ -65,15 +76,45 @@ const ILLEGAL: u64 = !0;
  is indexed with `(from<<6)+to`, where from and to are field numbers 0..63
 
  Each entry is a bit set that indicates which fields must be empty for making the move legal.
- Thus 0 means the move is legal (except when target field is occupied by a piece of the same color),
- whereas when all bits are set it indicates it is not legal (since it cannot be the case that all fields are empty).
+ Thus 0 means the move is legal, not considering occupation of the target field,
+ whereas when all bits are set it indicates it is not legal
+ since it cannot be the case that all fields are empty.
 
  Note that we don't have tables for queens, knights and kings.
- This is because queen moves are like either rook or bishop moves.
- Knights and kings, on the other hand, can never be blocked, hence it suffices
- to look in the kingTo table, for example, to find the set of fields a king can go and check if the target field is a member.
+ This is because queen moves are like either rook or bishop moves,
+ Hence the queenFromTo values would simply be the union of rookFromTo and bishopFromTo values.
+
+ Moves of knights and kings, on the other hand, can never be affected by the placement of
+ other pieces, hence all values would be LEGAL or ILLEGAL.
+
 */
 static mut bishopFromTo: [u64; 64 * 64] = [ILLEGAL; 64 * 64];
 static mut rookFromTo: [u64; 64 * 64] = [ILLEGAL; 64 * 64];
 static mut whitePawnFromTo: [u64; 64 * 64] = [ILLEGAL; 64 * 64];
 static mut blackPawnFromTo: [u64; 64 * 64] = [ILLEGAL; 64 * 64];
+
+/**
+ Set of fields that can in principle be reached directly
+ by a certain piece from a certain field.
+
+ This is indexed by a field number 0..63
+
+ The value can be used to find relevant entries in the ...FromTo tables, if applicable,
+ and finally check the free fields in the current position.
+*/
+static mut bishopTo: [u64; 64] = [0; 64];
+static mut rookTo: [u64; 64] = [0; 64];
+static mut knightTo: [u64; 64] = [0; 64];
+static mut kingTo: [u64; 64] = [0; 64];
+static mut whitePawnTo: [u64; 64] = [0; 64];
+static mut blackPawnTo: [u64; 64] = [0; 64];
+
+/**
+The inverse of whitePawnTo/blackPawnTo: if there is a black/white pawn on any of the
+indicated fields, then the given field is attacked.
+We need this only for pawns, since the moves of all other pieces are symmetric, that is,
+for example, the set of fields an index can be attacked from with a bishop is the same as
+the set of fields a bishop on index can go to.
+*/
+static mut whitePawnFrom: [u64; 64] = [0; 64];
+static mut blackPawnFrom: [u64; 64] = [0; 64];
