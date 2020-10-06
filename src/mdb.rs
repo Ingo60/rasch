7376812661ad00cc5f1b88c7c6ddc9130c04d89a
Ingo Wechsung <ingo.wechsung@gmail.com>
2020-10-06 19:27:57 +0200

    added genPawn()

--- src/mdb.rs
+++ src/mdb.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)] // sorry, need this because this is a rewrite of existing Java code
 #![allow(non_upper_case_globals)] // as well as this
 
-/**
+/*!
 # Move Data Base
 
 Functions for setting up tables that can later be used to find moves fast.
@@ -16,6 +16,8 @@ For Bishop/Rook, we return a bitmap of the fields that need to be free for the m
 valid. Hence, 0 means the move is possible, and -1 means it is not possible (unless the board was completly empty).
 
 */
+
+use super::fieldset::*;
 
 //                  Geometry
 //      8        7        6       5         4        3       2        1
@@ -118,3 +120,106 @@ the set of fields a bishop on index can go to.
 */
 static mut whitePawnFrom: [u64; 64] = [0; 64];
 static mut blackPawnFrom: [u64; 64] = [0; 64];
+
+/**
+     tell if we can go into a certain direction from some field
+*/
+pub fn canGo(from: u64, direction: i32) -> bool {
+    match direction {
+        EAST => (from & RECHTS) == 0,
+        WEST => (from & LINKS) == 0,
+        NORTH => (from & OBEN) == 0,
+        SOUTH => (from & UNTEN) == 0,
+        NE => (from & (OBEN | RECHTS)) == 0,
+        SE => (from & (UNTEN | RECHTS)) == 0,
+        NW => (from & (OBEN | LINKS)) == 0,
+        SW => (from & (UNTEN | LINKS)) == 0,
+        _ => false,
+    }
+}
+
+/// Go from some field in a certain direction
+/// * fld - a singleton set that denotes the field in question
+/// * direction - one of NORTH, EAST, SOUTH, WEST, NE, NW, SE, SW
+/// Returns the field that is one step from the given field in a given direction,
+///       if there is such a field, otherwise, nonsense results.
+/// This should only be done when canGo returns true
+
+pub fn goTowards(fld: u64, direction: i32) -> u64 {
+    if direction < 0 {
+        fld >> (-direction)
+    } else {
+        fld << direction
+    }
+}
+
+/// tell if we cab go 2 steps in the indicated direction
+pub fn canGo2(from: u64, direction: i32) -> bool {
+    canGo(from, direction) && canGo(goTowards(from, direction), direction)
+}
+
+/// convert a singleton to an index
+/// (actually, it looks only for the least significant 1 bit)
+/// beware not to pass 0, because then the index becomes 64
+pub fn setToIndex(singleton: u64) -> usize {
+    singleton.trailing_zeros() as usize
+}
+
+pub unsafe fn genPawn() {
+    let mut from = 0x100u64; // no pawns on base row
+    while from < 0x0100_0000_0000_0000u64 {
+        if canGo(from, NORTH) {
+            let to = goTowards(from, NORTH);
+            whitePawnTo[setToIndex(from)] |= to;
+            whitePawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = to;
+            if (from & 0xff00u64) != 0 && canGo(to, NORTH) {
+                // A2..H2 second rank
+                let mask = to;
+                let to = goTowards(to, NORTH);
+                whitePawnTo[setToIndex(from)] |= to;
+                whitePawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = to | mask;
+            }
+        }
+        if canGo(from, SOUTH) {
+            let to = goTowards(from, SOUTH);
+            blackPawnTo[setToIndex(from)] |= to;
+            blackPawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = to;
+            if (from & 0x00ff000000000000u64) != 0 && canGo(to, SOUTH) {
+                // A7..H7 seventh rank
+                let mask = to;
+                let to = goTowards(to, SOUTH);
+                blackPawnTo[setToIndex(from)] |= to;
+                blackPawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = to | mask;
+            }
+        }
+        if canGo(from, NE) {
+            let to = goTowards(from, NE);
+            whitePawnTo[setToIndex(from)] |= to;
+            whitePawnFrom[setToIndex(to)] |= from;
+            whitePawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = LEGAL;
+        }
+        if canGo(from, NW) {
+            let to = goTowards(from, NW);
+            whitePawnTo[setToIndex(from)] |= to;
+            whitePawnFrom[setToIndex(to)] |= from;
+            whitePawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = LEGAL;
+        }
+        if canGo(from, SE) {
+            let to = goTowards(from, SE);
+            blackPawnTo[setToIndex(from)] |= to;
+            blackPawnFrom[setToIndex(to)] |= from;
+            blackPawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = LEGAL;
+        }
+        if canGo(from, SW) {
+            let to = goTowards(from, SW);
+            blackPawnTo[setToIndex(from)] |= to;
+            blackPawnFrom[setToIndex(to)] |= from;
+            blackPawnFromTo[(setToIndex(from) << 6) + setToIndex(to)] = LEGAL;
+        }
+        from <<= 1;
+    }
+}
+
+pub fn canWhitePawn(from: Field, to: Field) -> BitSet {
+    unsafe { BitSet::from(whitePawnFromTo[((from as usize) << 6) + (to as usize)]) }
+}
