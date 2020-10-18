cc2bf28231f14b97d74d7aa8878af4b29c37aff4
Ingo Wechsung <ingo.wechsung@gmail.com>
2020-10-18 19:46:39 +0200

    completed strategy_negamin, negaSimple. Todo negaMax

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(non_snake_case)]
 
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::io;
 use std::io::Write;
@@ -13,7 +14,7 @@ use rasch::common::Protocol::*;
 // use rasch::common::Variation;
 use rasch::common::*;
 // use rasch::fen;
-// use rasch::computing as C;
+use rasch::computing;
 // use rasch::fieldset::*;
 use rasch::mdb;
 use rasch::position as P;
@@ -58,10 +59,11 @@ pub fn strategy_best(mut state: StrategyState) {
             .send(MV(
                 state.sid,
                 Variation {
-                    depth: 1,
-                    moves: vec![mv],
-                    nodes: 1,
-                    score: p2.eval(),
+                    depth:  1,
+                    moves:  [mv; VariationMoves as usize],
+                    length: 1,
+                    nodes:  1,
+                    score:  p2.eval(),
                 },
             ))
             .unwrap();
@@ -113,8 +115,8 @@ pub fn orderMoves(pos: &Position, moves: &[Move]) -> Vec<Move> {
 /// Correct a variation score of mate to slightly smaller value
 /// so that shorter ways to mate have a better score than longer ones.
 pub fn correctMateDistance(var: &Variation) -> Variation {
-    let dist = var.moves.len() as i32 * 2;
-    let mut pv = var.clone();
+    let dist = var.length as i32 * 2;
+    let mut pv = *var;
     if pv.score == P::whiteIsMate {
         pv.score += dist
     } else if pv.score == P::blackIsMate {
@@ -138,7 +140,7 @@ pub fn insertTest(mut hash: TransTable) {
 
 /// Move searching with NegaMax
 pub fn negaMax(
-    state: &mut StrategyState,
+    hist: &mut Vec<Position>,
     mut hash: TransTable,
     extended: bool,
     depth: u32,
@@ -146,7 +148,46 @@ pub fn negaMax(
     beta: i32,
 ) -> Variation {
     insertTest(hash);
-    state.plan.as_ref().unwrap().clone()
+    Variation {
+        depth:  1,
+        nodes:  1,
+        score:  1234,
+        moves:  [P::noMove; VariationMoves],
+        length: 1,
+    }
+}
+
+/// Iterative deepening for negaMax
+pub fn negaSimple(mut state: StrategyState, depth: u32, alpha: i32, beta: i32) {
+    let mut depth = depth;
+    println!("depth {}", depth);
+    loop {
+        if computing::thinkingFinished() {
+            state.tellNoMore();
+            break;
+        }
+        // acquire mutable access to the transposition table
+        // the hash is locked during search
+        let mut hash: TransTable = state.trtable.lock().unwrap();
+        let mut hist = state.history.clone();
+        let mut pv = correctMateDistance(&negaMax(&mut hist, hash, false, depth, -beta, -alpha));
+        pv.score *= state.player().factor();
+        pv.depth = depth;
+        if state.talkPV(pv) {
+            if pv.length + 1 < depth
+                || pv.score <= P::whiteIsMate + 2
+                || pv.score >= P::blackIsMate - 2
+                || pv.score == 0
+            {
+                state.tellNoMore();
+                break;
+            } else {
+                depth += 1;
+            }
+        } else {
+            break;
+        }
+    }
 }
 
 /// search with the *negamin* algorithm
@@ -158,66 +199,101 @@ pub fn strategy_negamin(mut state: StrategyState) {
     );
     io::stdout().flush().unwrap_or_default();
 
-    // acquire mutable access to the transposition table
-    let mut hash: TransTable = state.trtable.lock().unwrap(); // the hash is locked all the time
+    // we need this all the time
+    let factor = state.player().factor();
 
     // if there's just 1 move left, we have no choice
     if allMoves.len() == 1 {
         if state.talkPV(Variation {
-            depth: 1,
-            nodes: 1,
-            score: state.player().factor() * (-9999),
-            moves: allMoves,
+            depth:  1,
+            nodes:  1,
+            score:  factor * (-9999),
+            moves:  [allMoves[0]; VariationMoves as usize],
+            length: 1,
         }) {
             state.tellNoMore();
         }
         return;
     };
     // we do have more than 1 move at this point
-    match &state.plan {
+    match state.plan {
         // If the plan promises a mate in a few moves, we unconditionally play it.
         // Note that if this is so, the 50 moves rule doesn't get in the way.
-        Some(plan) if plan.moves.len() > 2 && P::blackIsMate - 100 < plan.score * state.player().factor() => {
-            println!("We have a mate plan: {}", P::showMoves(&plan.moves[2..]));
+        Some(plan) if plan.length > 2 && plan.score * factor > P::blackIsMate - 100 => {
+            println!("# We have a mate plan: {}", P::showMoves(&plan.moves[2..]));
             io::stdout().flush().unwrap_or_default();
-            if state.talkPV(Variation {
-                depth: plan.depth - 2,
-                nodes: 1,
-                moves: plan.moves[2..].iter().copied().collect(),
-                ..*plan
-            }) {
+            if state.talkPV(
+                Variation {
+                    depth: plan.depth - 2,
+                    nodes: 1,
+                    moves: [P::noMove; VariationMoves],
+                    ..plan
+                }
+                .from_iter(&mut plan.moves().into_iter().skip(2).copied()),
+            ) {
                 state.tellNoMore();
             };
             return;
         }
         // The plan should promise a win of at least 1/4 pawn.
-        Some(plan) if plan.moves.len() > 2 && plan.score * state.player().factor() > 25 => {
+        Some(plan) if plan.length > 2 && plan.score * factor > 25 => {
             let planmv = plan.moves[2];
-            println!("We have a plan: {}", P::showMoves(&plan.moves[2..]));
+            println!("# We have a plan: {}", P::showMoves(&plan.moves[2..]));
             io::stdout().flush().unwrap_or_default();
-            if !state.talkPV(Variation {
-                depth: plan.depth - 2,
-                nodes: 1,
-                moves: plan.moves[2..].iter().copied().collect(),
-                ..*plan
-            }) {
+            if !state.talkPV(
+                Variation {
+                    depth: plan.depth - 2,
+                    nodes: 1,
+                    moves: [P::noMove; VariationMoves],
+                    ..plan
+                }
+                .from_iter(&mut plan.moves().into_iter().skip(2).copied()),
+            ) {
                 // for some reason, we must stop
                 return;
             };
             // re-search the move
             let pos = state.current().apply(planmv);
-            let mut pv = Variation {
-                nodes: 0,
-                depth: 0,
-                score: 0,
-                moves: Vec::with_capacity(16),
+            let mut hist = state.history.clone();
+            hist.push(pos);
+            let pv2 = {
+                // acquire mutable access to the transposition table
+                // the hash is locked during search
+                let mut hash: TransTable = state.trtable.lock().unwrap();
+
+                correctMateDistance(&negaMax(
+                    &mut hist,
+                    hash,
+                    false,
+                    min(8, plan.depth - 1),
+                    P::whiteIsMate,
+                    P::blackIsMate,
+                ))
             };
-            pv.moves.push(planmv);
-            state.history.push(pos);
-            // TODO: continue with negaMax
+            let mut pv = pv2.unshift(planmv);
+            pv.score *= pos.turn().factor();
+            pv.depth = plan.depth;
+            println!("# Plan continuation: {}", pv.showMoves());
+            io::stdout().flush().unwrap_or_default();
+            let cont = state.talkPV(pv);
+            let newscore = factor * pv.score;
+            if newscore > P::percent(95, factor * plan.score)
+                && (newscore == P::blackIsMate || newscore <= P::blackIsMate - 100)
+            {
+                println!("# Plan looks promising.");
+                state.tellNoMore();
+            } else {
+                println!("# We better look for another plan.");
+                if cont {
+                    negaSimple(state, plan.depth, P::whiteIsMate, P::blackIsMate);
+                }
+            }
+            io::stdout().flush().unwrap_or_default();
         }
         _ => {
-            // TODO: processing without a plan
+            println!("We do not have a plan.");
+            io::stdout().flush().unwrap_or_default();
+            negaSimple(state, 4, P::whiteIsMate, P::blackIsMate);
         }
     };
 }
